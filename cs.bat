a9fc3e07f05876381ece53306d7eaf6eab61462d
Vladimir <vladimir0ne1@gmail.com>
2013-12-12 10:45:09 +0300

    Added create site .bat file v1.0

---
+++ cs.bat
@@ -0,0 +1,10 @@
+REM v1.0
+REM Run this batch file in folder with your .NET site
+REM first parameter - sitename
+
+appcmd add site /name:%1 /physicalPath:%CD% /bindings:http/*:85:%1
+appcmd add apppool /name:%1 /managedRuntimeVersion:v4.0
+appcmd set site /site.name:%1 /[path='/'].applicationPool:%1
+echo.
+echo 127.0.0.1	%1 >> %WINDIR%\System32\Drivers\Etc\Hosts
+echo.
