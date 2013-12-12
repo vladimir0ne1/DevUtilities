61d995c58592da987dbcd6d6224b317d610f423d
Vladimir <vladimir0ne1@gmail.com>
2013-12-12 10:52:11 +0300

    fixed port bug

--- cs.bat
+++ cs.bat
@@ -2,7 +2,7 @@ REM v1.0
 REM Run this batch file in folder with your .NET site
 REM first parameter - sitename
 
-appcmd add site /name:%1 /physicalPath:%CD% /bindings:http/*:85:%1
+appcmd add site /name:%1 /physicalPath:%CD% /bindings:http/*:80:%1
 appcmd add apppool /name:%1 /managedRuntimeVersion:v4.0
 appcmd set site /site.name:%1 /[path='/'].applicationPool:%1
 echo.
